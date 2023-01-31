323618aff3f20115f889fd79680462218f535bfd
fredymarb <29468132+fredymarb@users.noreply.github.com>
2023-01-31 21:27:26 +0000

    Added a counter to keep track of game score

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     //Returns a random whole number between 1 and 3
     let computerChoice = Math.floor((Math.random() * 3) + 1);
@@ -20,44 +24,56 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     // User win outcomes 
     if(playerSelection === "rock" && computerSelection === "scissors") { 
-        return "You win this round!";
+        playerScore += 1;
+        return "YOU WIN";
     } 
     else if (playerSelection === "scissors" && computerSelection === "paper") { 
-        return "You win this round!";
+        playerScore += 1;
+        return "YOU WIN";
     } 
     else if (playerSelection === "paper" && computerSelection === "rock") { 
-        return "You win this round!";
+        playerScore += 1;
+        return "YOU WIN";
     } 
 
     //User Lose outcomes
-    else if(playerSelection === "rock" && computerSelection === "paper") { 
-        return "You lose this round!";
+    else if(playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1;
+        return "YOU LOSE";
     }
     else if(playerSelection === "scissors" && computerSelection === "rock") { 
-        return "You lose this round!";
+        computerScore += 1;
+        return "YOU LOSE";
     }
     else if(playerSelection === "paper" && computerSelection === "scissors") { 
-        return "You lose this round!";
+        computerScore += 1;
+        return "YOU LOSE";
     }
 
     //Tie outcomes 
     else  if(playerSelection === computerSelection){ 
-        return "It's a tie!";
+        return "TIE";
     }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
+    //This for loop runs forever (the INFINITY loop)
+    for (let i = 0; i < Infinity; i++) {
         let getUserChoice = () => (prompt("Choose either rock paper or scissors", 'rock')).toLowerCase();
         
         let playerSelection = getUserChoice();
         let computerSelection = getComputerChoice();
 
         console.log(`Player: ${playerSelection}, computer: ${computerSelection}`);
-        // console.log(`Computer value is: ${computerSelection}`);
         console.log(playRound(playerSelection, computerSelection));
+
+        // IMPORTANT CODE HERE 
+        // This condition breaks the Infinity loop if either score counts to 5 first
+        let roundsPerGame = 5;
+        if (computerScore == roundsPerGame || playerScore == roundsPerGame) {break; } 
     }
 
+    console.log(`computerScore: ${computerScore}, playerScore: ${playerScore}`);
 }
 
 game();
